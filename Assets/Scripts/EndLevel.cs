a206931164a7724766b89edd722c3e1a78a0337d
daniimudaa <31983241+daniimudaa@users.noreply.github.com>
2017-11-19 02:31:33 +1100

    Dani's changes - ALOT of stuff

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour 
+{
+	void OnTriggerEnter (Collider col) 
+	{
+		if (col.transform.tag == "Player") 
+		{
+			Time.timeScale = 0;
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
---
+++ Assets/Scripts/ShelfBreak.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelfBreak : MonoBehaviour 
+{
+	public GameObject shelf;
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.transform.tag == "Player") 
+		{
+			shelf.SetActive (false);
+		}
+	}
+}
