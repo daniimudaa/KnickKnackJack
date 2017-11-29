a53d977c4cd3d72bc8a7b628927fbebad80df141
daniimudaa <31983241+daniimudaa@users.noreply.github.com>
2017-11-29 14:47:24 +1100

    Lounge 2 - Animations

---
+++ Assets/AutomaticPlay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticPlay : MonoBehaviour 
+{
+	public Animation anim;
+	public Animation anim1;
+	public Animation anim2;
+	public Animation anim3;
+	public Animation anim4;
+	public Animation anim5;
+	public Animation anim6;
+	public Animation anim7;
+	public Animation anim8;
+
+	void Update () 
+	{
+		StartCoroutine (MyAnimations ());
+	}
+
+	IEnumerator MyAnimations()
+	{
+		anim.Play ();
+		yield return new WaitForSeconds(4);
+		anim1.Play ();
+		yield return new WaitForSeconds(4);
+		anim2.Play ();
+		yield return new WaitForSeconds(4);
+		anim3.Play ();
+		yield return new WaitForSeconds(4);
+		anim4.Play ();
+		yield return new WaitForSeconds(4);
+		anim5.Play ();
+		yield return new WaitForSeconds(4);
+		anim6.Play ();
+		yield return new WaitForSeconds(4);
+		anim7.Play ();
+		yield return new WaitForSeconds(4);
+		anim8.Play ();
+		yield return new WaitForSeconds(4);
+
+	}
+}
