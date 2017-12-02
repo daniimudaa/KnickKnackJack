500748f6085e68f854d141ca3cb2ef4f7925bc4e
daniimudaa <31983241+daniimudaa@users.noreply.github.com>
2017-12-03 00:58:19 +1100

    New

--- Assets/Scripts/Menus/MainMenus.cs
+++ Assets/Scripts/Menus/MainMenus.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using GamepadInput;
 
 public class MainMenus : MonoBehaviour
 {
@@ -7,20 +8,104 @@ public class MainMenus : MonoBehaviour
 
 	public GameObject pauseMenu;
 
+	public bool controlScreen;
+
+	public GameObject gnomeControlMenu;
+	public GameObject teddyControlMenu;
+	public GameObject robotControlMenu;
+	public GameObject dollControlMenu;
+
+	public GameObject turnoffUI_1;
+	public GameObject turnoffUI_2;
+	public GameObject turnoffUI_3;
+	public GameObject turnoffUI_4;
+	public GameObject turnoffUI_5;
+
+
     public void Start()
     {
         Time.timeScale = 1;
+
+		controlScreen = false;
     }
+
+	public void Update ()
+	{
+		BackButton ();
+		ControlsMenuChange ();
+	}
 
     public void Play()
     {
         SceneController.LoadScene("Scenes/Menus/PlayerConfiguration");
     }
 
-    public void Instructions()
+    public void Controls()
     {
-        SceneController.LoadScene("InstructionScene");
+		turnoffUI_1.SetActive(false);
+		turnoffUI_2.SetActive(false);
+		turnoffUI_3.SetActive(false);
+		turnoffUI_4.SetActive(false);
+		turnoffUI_5.SetActive(false);
+
+		controlScreen = true; 
+		gnomeControlMenu.SetActive(true);
     }
+
+	public void BackButton()
+	{
+		if (GamePad.GetButtonDown(GamePad.Button.B, GamePad.Index.Any)) 
+		{
+			
+			gnomeControlMenu.SetActive (false);
+			teddyControlMenu.SetActive (false);
+			robotControlMenu.SetActive (false);
+			dollControlMenu.SetActive (false);
+
+			turnoffUI_1.SetActive(true);
+			turnoffUI_2.SetActive(true);
+			turnoffUI_3.SetActive(true);
+			turnoffUI_4.SetActive(true);
+			turnoffUI_5.SetActive(true);
+
+			controlScreen = false;
+		}
+	}
+
+	public void ControlsMenuChange()
+	{
+		if (controlScreen && GamePad.GetButtonDown (GamePad.Button.A, GamePad.Index.One)) 
+		{
+			gnomeControlMenu.SetActive (true);
+			teddyControlMenu.SetActive (false);
+			robotControlMenu.SetActive (false);
+			dollControlMenu.SetActive (false);
+		}
+
+		if (controlScreen && GamePad.GetButtonDown (GamePad.Button.A, GamePad.Index.Two))  
+		{
+			gnomeControlMenu.SetActive (false);
+			teddyControlMenu.SetActive (true);
+			robotControlMenu.SetActive (false);
+			dollControlMenu.SetActive (false); 
+		}
+
+		if (controlScreen && GamePad.GetButtonDown (GamePad.Button.A, GamePad.Index.Three))  
+		{
+			gnomeControlMenu.SetActive (false);
+			teddyControlMenu.SetActive (false);
+			robotControlMenu.SetActive (true);
+			dollControlMenu.SetActive (false);
+		}
+
+		if (controlScreen && GamePad.GetButtonDown (GamePad.Button.A, GamePad.Index.Four))  
+		{
+			gnomeControlMenu.SetActive (false);
+			teddyControlMenu.SetActive (false);
+			robotControlMenu.SetActive (false);
+			dollControlMenu.SetActive (true);
+		}
+	}
 
     public void QuitGame()
     {
@@ -48,4 +133,9 @@ public class MainMenus : MonoBehaviour
     {
         SceneController.LoadScene("MainMenu");
     }
+
+	public void Credits()
+	{
+		SceneController.LoadScene("Credits");
+	}
 }
