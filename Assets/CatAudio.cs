8a7ccbf8972f7940ab33b9373bb12b584360f27a
daniimudaa <31983241+daniimudaa@users.noreply.github.com>
2017-12-10 23:46:08 +1100

    animfix

---
+++ Assets/CatAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAudio : MonoBehaviour
+{
+	public AudioSource source;
+	public AudioClip[] clips;
+
+	public void OnTriggerEnter (Collider col)
+	{
+		if (col.transform.tag == "Player") 
+		{
+			source.clip = clips [Random.Range (1, clips.Length)];  
+			source.Play ();
+		}
+	}
+}
--- Assets/Scripts/Environment/Projectile/AnimateOnHit.cs
+++ Assets/Scripts/Environment/Projectile/AnimateOnHit.cs
@@ -3,9 +3,12 @@
 public class AnimateOnHit : ProjectileHandler
 {
     public Animation anim;
+	public GameObject fruit;
 
     public override void OnHit()
     {
         anim.Play();
+		fruit.GetComponent<Rigidbody> ().isKinematic = true;
+		fruit.GetComponent<Rigidbody> ().isKinematic = false;
     }
 }
