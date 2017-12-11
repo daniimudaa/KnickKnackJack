cd9a1adb19647c57fcf9449ba56a39c55fd57ab9
daniimudaa <31983241+daniimudaa@users.noreply.github.com>
2017-12-11 16:53:45 +1100

    particles

--- Assets/Scripts/Environment/BreakableCube.cs
+++ Assets/Scripts/Environment/BreakableCube.cs
@@ -1,22 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class BreakableCube : MonoBehaviour {
+public class BreakableCube : MonoBehaviour 
+{
+	public GameObject explosionParticle;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void OnTriggerEnter(Collider other) 
+	IEnumerator OnTriggerEnter(Collider other) 
 	{
 		if (other.gameObject.tag == "Axe")
 		{
+			GameObject clone = Instantiate(explosionParticle, gameObject.transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(0.1f);
 			Destroy (gameObject);
+			yield return new WaitForSeconds(5);
+
+			Destroy(clone, 4.0f);
 		}
 	}
 }
